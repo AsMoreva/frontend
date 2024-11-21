969465ebcf256667b2f3ce7b1279f2a42bf7edf2
AsMoreva <160594079+AsMoreva@users.noreply.github.com>
2024-11-22 02:34:52 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ChangePassword from './components/ChangePassword';
+import Dashboard from './components/Dashboard';
 import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
-import Dashboard from './components/Dashboard';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-	<Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/change-password" element={<ChangePassword />} /> {/* Страница изменения пароля */}
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
